14ccb126bce84fef64cf23a954db4afbbb4168e4
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-09 00:54:27 -0300

    :recycle: feat: remove `src/api`

--- src/api/routes/hello.ts
+++
@@ -1,8 +0,0 @@
-import HelloController from '@controllers/hello'
-import { Router } from 'express'
-
-const router = Router()
-
-router.get('/', HelloController.sayHello)
-
-export default router
--- src/api/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from 'express'
-import hello from './hello'
-
-// TODO: Automate the routes. like forEach
-function addApiRoutes (app: Application): void {
-  app.use('/', hello)
-}
-
-export default addApiRoutes
