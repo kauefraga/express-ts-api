dbebeaf57d4bf8ca47136069791a576256a102c0
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-09 22:39:41 -0300

    :sparkles: feat: add `NotFoundMiddleware` to handle 404 routes

---
+++ src/middlewares/404.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { ApiResponse } from '../types/ApiResponse';
+
+export function NotFoundMiddleware(
+  _: Request,
+  response: Response<ApiResponse>
+) {
+  return response.status(404).json({
+    success: false,
+    data: {
+      error: {
+        name: 'Not Found',
+        message: 'The requested URL is not available.',
+      },
+      now: new Date(),
+    },
+  });
+}
