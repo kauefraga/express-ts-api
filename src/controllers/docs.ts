d6ffb9f4ed01225d91b760e326d07c78f53a750f
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-10 12:20:30 -0300

    :sparkles: feat: add `/v1/docs` route

---
+++ src/controllers/docs.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { ApiResponse } from '../types/ApiResponse';
+
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333'
+    : 'https://production.url';
+
+export class DocsController {
+  static handler(
+    _: Request,
+    response: Response<ApiResponse>
+  ) {
+    return response.status(200).json({
+      success: true,
+      data: {
+        available: [
+          {
+            url: `${baseUrl}/v1`,
+            alias: '/api/v1',
+            method: 'GET',
+          },
+          {
+            url: `${baseUrl}/v1/docs`,
+            alias: '/api/v1/docs',
+            method: 'GET',
+          },
+        ],
+        now: new Date(),
+      },
+    });
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { HelloController } from './controllers/hello';
+import { HelloController } from '@controllers/hello';
+import { DocsController } from '@controllers/docs';
 
 const router = Router();
 
 router.get(['/v1', '/api/v1'], HelloController.handler);
+
+router.get(
+  ['/v1/docs', '/api/v1/docs'],
+  DocsController.handler
+);
 
 export { router };
