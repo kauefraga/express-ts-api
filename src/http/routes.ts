81a754d7669cd52b3f6bdaa2b4bc1aa8ccc8d7b9
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-08 17:45:53 -0300

    :fire: remove docs route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,16 +1,10 @@
 import { Router } from 'express';
 import { HelloController } from './controllers/hello';
-import { DocsController } from './controllers/docs';
 import { CreateUserController } from './controllers/users/create';
 
 const router = Router();
 
 router.get('/v1', HelloController);
-
-router.get(
-  '/v1/docs',
-  DocsController,
-);
 
 router.post(
   '/v1/users/create',
