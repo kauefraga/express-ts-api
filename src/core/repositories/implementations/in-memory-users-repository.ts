6be29bc24c70a11d72432d8da9d71d7de59688f9
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-08 17:48:28 -0300

    :recycle: chore: change it to be more simpler

--- src/core/repositories/implementations/in-memory-users-repository.ts
+++ src/core/repositories/implementations/in-memory-users-repository.ts
@@ -7,22 +7,4 @@ export class InMemoryUsersRepository implements UsersRepository {
   async create(user: User) {
     this.users.push(user);
   }
-
-  async findById(id: string) {
-    const foundUser = this.users.find((user) => user.id === id);
-
-    return foundUser || null;
-  }
-
-  async findByName(name: string) {
-    const foundUser = this.users.find((user) => user.name === name);
-
-    return foundUser || null;
-  }
-
-  async delete(name: string) {
-    const foundUser = await this.findByName(name);
-
-    this.users.filter((user) => user.name !== foundUser?.name);
-  }
 }
